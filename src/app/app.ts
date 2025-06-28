39f3c79c3e4f10f306316a0be982c034a7e09fa9
Lucas Fernandes <lucassilvafernandes2014@gmail.com>
2025-06-28 15:12:49 -0300

    Funcionalidade de logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
-import { AuthService } from './services/auth';
+import { AuthService } from './services/auth.service';
+import { SessionService } from './services/session.service';
+import { TokenExpiredCheckService } from './services/token-expired-check.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,36 @@ import { AuthService } from './services/auth';
   templateUrl: './app.html',
   styleUrl: './app.css',
   providers: [
-    AuthService
+    AuthService,
+    SessionService
   ]
 })
 export class App {
   protected title = 'pfinances';
+
+  constructor(
+    private sessionService: SessionService,
+    private token: TokenExpiredCheckService,
+    private router: Router
+  ) {}
+
+  async logout() {
+    let status = await this.sessionService.deleteSession();
+    switch(status) {
+      case 201:
+        localStorage.removeItem('pFinancesAccessToken');
+        localStorage.removeItem('pFinancesFamilyId');
+        localStorage.removeItem('pFinancesRole');
+        localStorage.removeItem('pFinancesUserEmailAddress');
+        localStorage.removeItem('pFinancesUserId');
+        localStorage.removeItem('pFinancesUserName');
+        this.router.navigate(['login']);
+        break;
+      default:
+        console.log('Erro ao excluir sessão, o serviço retornou erro '+status);
+        //VERIFICA SE O TOKEN EXPIROU
+        this.token.checkWetherTokenExpired(status);
+        break;
+    }
+  }
 }
