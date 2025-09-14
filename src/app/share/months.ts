e9aaa13243d4ee5a2fd363654a85cd550446d7f8
Lucas Fernandes <lucassilvafernandes2014@gmail.com>
2025-09-14 02:53:31 -0300

    Funcionalidade Consultar Receitas

--- src/app/share/months.ts
+++ src/app/share/months.ts
@@ -47,4 +47,51 @@ export class Months {
 
         return monthNumber;
     }
+
+    public convertMonthNumberToMonthName(month: string) {
+
+        var monthName = '';
+        
+        switch(month) {
+            case '01':
+                monthName = 'Janeiro';
+                break;
+            case '02':
+                monthName = 'Fevereiro';
+                break;
+            case '03':
+                monthName = 'Março';
+                break;
+            case '04':
+                monthName = 'Abril';
+                break;
+            case '05':
+                monthName = 'Maio';
+                break;
+            case '06':
+                monthName = 'Junho';
+                break;
+            case '07':
+                monthName = 'Julho';
+                break;
+            case '08':
+                monthName = 'Agosto';
+                break;
+            case '09':
+                monthName = 'Setembro';
+                break;
+            case '10':
+                monthName = 'Outubro';
+                break;
+            case '11':
+                monthName = 'Novembro';
+                break;
+            default:
+                monthName = 'Dezembro';
+                break;
+                
+        }
+
+        return monthName;
+    }
 }
